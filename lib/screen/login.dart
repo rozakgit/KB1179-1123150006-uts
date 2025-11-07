4e9907c53b39428d4e27c45af7522382003b9abb
rozakgit <muhammadabdulrozak21@gmail.com>
2025-11-07 20:14:00 +0700

    menambahkan code di login screen,sizebox,icon,dll

--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -5,4 +5,67 @@ import 'package:kb1179_1123150006_uts/screen/splash_screen_1.dart';
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
-  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.all(20.0),
+            child: Column(
+              children: [
+                SizedBox(height: 10),
+                Icon(Icons.lock_outlined, size: 80, color: Colors.deepPurple),
+                SizedBox(height: 10),
+                Text(
+                  'Selamat Datang',
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  'Silahkan login untuk melanjutkan',
+                  style: TextStyle(fontSize: 15, color: Colors.deepPurple),
+                ),
+                SizedBox(height: 50),
+                TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.email_outlined),
+                    label: Text("Email"),
+                    hint: Text("Masukkan email Anda"),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(12)),
+                      borderSide: BorderSide(color: Colors.grey[300]!),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(12)),
+                      borderSide: BorderSide(
+                        color: Colors.deepPurple[700]!,
+                        width: 2,
+                      ),
+                    ),
+                    // hintText: "halo",
+                  ),
+                ),
+                SizedBox(height: 20),
+                TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.lock_outline),
+                    suffixIcon: Icon(Icons.visibility_outlined),
+                    label: Text("Password"),
+                    hintText: 'Masukkan password Anda',
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(12)),
+                      borderSide: BorderSide(color: Colors.grey[300]!),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(12)),
+                      borderSide: BorderSide(
+                        color: Colors.deepPurple[700]!,
+                        width: 2,
+                      ),
+                    ),
+                  ),
+                ),
+               
