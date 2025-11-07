c91c5de42e3e7083da38a73a249b44ab98949272
rozakgit <muhammadabdulrozak21@gmail.com>
2025-11-07 20:10:01 +0700

    menambahkan code class login

---
+++ lib/screen/login.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:kb1179_1123150006_uts/main.dart';
+import 'package:kb1179_1123150006_uts/screen/splash_screen_1.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  
