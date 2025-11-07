c2cc9e83baaab471fd45328e2a581c86dfd89d20
rozakgit <muhammadabdulrozak21@gmail.com>
2025-11-07 20:07:34 +0700

    menambahkan code di splash 3

--- lib/screen/splash_screen_3.dart
+++ lib/screen/splash_screen_3.dart
@@ -8,4 +8,127 @@ class SplashScreen3 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-     
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Spacer(),
+            SizedBox(height: 50),
+            Container(
+              width: 250,
+              height: 250,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.cyan,
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: AssetImage('assets/images/image3.png'),
+                ),
+              ),
+            ),
+            //tambahkan disini untuk widget widget lainnya nanti
+            SizedBox(height: 30),
+            Text(
+              'Selamat Datang',
+              style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20),
+            Text(
+              textAlign: TextAlign.center,
+              'Ayo coba Wallie sekarang! Dan dapatkan solusi terbaik!',
+              style: TextStyle(fontSize: 20, color: Colors.grey[700]),
+            ),
+            SizedBox(height: 50),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: 12,
+                  height: 12,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.grey,
+                  ),
+                ),
+                SizedBox(width: 10),
+                Container(
+                  width: 12,
+                  height: 12,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.grey,
+                  ),
+                ),
+                SizedBox(width: 10),
+                Container(
+                  width: 12,
+                  height: 12,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.cyan,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 50),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 40),
+              child: SizedBox(
+                height: 50,
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => LoginScreen()),
+                    );
+                  },
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.cyan),
+                    elevation: MaterialStateProperty.resolveWith<double>((
+                      states,
+                    ) {
+                      return states.contains(MaterialState.pressed) ? 2.0 : 8.0;
+                    }),
+                    padding: MaterialStateProperty.resolveWith<EdgeInsetsGeometry>((
+                      states,
+                    ) {
+                      // slightly reduce vertical padding when pressed to give "pressed-in" look
+                      return states.contains(MaterialState.pressed)
+                          ? EdgeInsets.symmetric(
+                              horizontal: 50.0,
+                              vertical: 12.0,
+                            )
+                          : EdgeInsets.symmetric(
+                              horizontal: 50.0,
+                              vertical: 15.0,
+                            );
+                    }),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30.0),
+                      ),
+                    ),
+                    overlayColor: MaterialStateProperty.resolveWith<Color?>((
+                      states,
+                    ) {
+                      if (states.contains(MaterialState.pressed))
+                        return Colors.black.withOpacity(0.12);
+                      return null;
+                    }),
+                    shadowColor: MaterialStateProperty.all(Colors.cyan),
+                  ),
+                  child: Text(
+                    'AYO MULAI',
+                    style: TextStyle(fontSize: 18.0, color: Colors.white),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 70),
+          ],
+        ),
+      ),
+    );
+  }
+}
