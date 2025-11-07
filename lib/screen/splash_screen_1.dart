87b51b629934f000317b28123277b9307735ac9b
rozakgit <muhammadabdulrozak21@gmail.com>
2025-11-07 19:57:04 +0700

    menambahkan code di spalsh 1

---
+++ lib/screen/splash_screen_1.dart
@@ -0,0 +1,133 @@
+import 'package:flutter/material.dart';
+import 'package:kb1179_1123150006_uts/main.dart';
+import 'package:kb1179_1123150006_uts/screen/splash_screen_2.dart';
+
+class SplashScreen1 extends StatelessWidget {
+  const SplashScreen1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Spacer(),
+            SizedBox(height: 50),
+            Container(
+              width: 250,
+              height: 250,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.cyan,
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: AssetImage('assets/images/image1.png'),
+                ),
+              ),
+            ),
+            //tambahkan disini untuk widget widget lainnya nanti
+            SizedBox(height: 30),
+            Text(
+              'Selamat Datang',
+              style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Lupa Membawa Dompet?',
+              style: TextStyle(fontSize: 20, color: Colors.grey[700]),
+            ),
+            SizedBox(height: 50),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: 12,
+                  height: 12,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.cyan,
+                  ),
+                ),
+                SizedBox(width: 10),
+                Container(
+                  width: 12,
+                  height: 12,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.grey,
+                  ),
+                ),
+                SizedBox(width: 10),
+                Container(
+                  width: 12,
+                  height: 12,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.grey,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 50),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 40),
+              child: SizedBox(
+                height: 50,
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => SplashScreen2()),
+                    );
+                  },
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.cyan),
+                    elevation: MaterialStateProperty.resolveWith<double>((
+                      states,
+                    ) {
+                      return states.contains(MaterialState.pressed) ? 2.0 : 8.0;
+                    }),
+                    padding: MaterialStateProperty.resolveWith<EdgeInsetsGeometry>((
+                      states,
+                    ) {
+                      // slightly reduce vertical padding when pressed to give "pressed-in" look
+                      return states.contains(MaterialState.pressed)
+                          ? EdgeInsets.symmetric(
+                              horizontal: 50.0,
+                              vertical: 12.0,
+                            )
+                          : EdgeInsets.symmetric(
+                              horizontal: 50.0,
+                              vertical: 15.0,
+                            );
+                    }),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30.0),
+                      ),
+                    ),
+                    overlayColor: MaterialStateProperty.resolveWith<Color?>((
+                      states,
+                    ) {
+                      if (states.contains(MaterialState.pressed))
+                        return Colors.black.withOpacity(0.12);
+                      return null;
+                    }),
+                    shadowColor: MaterialStateProperty.all(Colors.cyan),
+                  ),
+                  child: Text(
+                    'Continue',
+                    style: TextStyle(fontSize: 18.0, color: Colors.white),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 70),
+          ],
+        ),
+      ),
+    );
+  }
+}
