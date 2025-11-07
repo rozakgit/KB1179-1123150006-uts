7e1932b98e41fbb0362fcdf2c87856357452046d
rozakgit <muhammadabdulrozak21@gmail.com>
2025-11-07 20:06:04 +0700

    menambahkan code widget build di splash 3

---
+++ lib/screen/splash_screen_3.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:kb1179_1123150006_uts/main.dart';
+import 'package:kb1179_1123150006_uts/screen/login.dart';
+
+class SplashScreen3 extends StatelessWidget {
+  const SplashScreen3({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+     
